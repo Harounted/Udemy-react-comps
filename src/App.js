4f914edc33c9ba58cba3f37a22e6547126a038d3
Haroun Ted <harounted@gmail.com>
2023-09-16 15:34:54 +0300

    done with the buttons

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import Button from "./Button";
+import { GoBell, GoDownload, GoDatabase } from "react-icons/go";
+
+const handleClick = () => {
+  console.log("click!");
+};
 
 function App() {
   return (
     <div>
       <div>
-        <Button success rounded outline>
-          Buy Now !
+        <Button primary outline rounded>
+          <GoBell />
+          Buy
         </Button>
       </div>
       <div>
         <Button danger outline>
+          <GoDownload />
           Add to cart
         </Button>
       </div>
       <div>
-        <Button warning>Delete</Button>
+        <Button warning>
+          <GoDatabase />
+          Delete
+        </Button>
       </div>
       <div>
         <Button secondary outline>
@@ -22,7 +32,7 @@ function App() {
         </Button>
       </div>
       <div>
-        <Button primary rounded>
+        <Button success rounded>
           Something else !
         </Button>
       </div>

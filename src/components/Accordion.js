35e89442e88ee7974cdb8cb4e40e694a142a22d3
Haroun Ted <harounted@gmail.com>
2023-09-17 14:47:19 +0300

    add event handlers to accordion

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,9 +1,19 @@
+import { useState } from "react";
+
 function Accordion({ items }) {
-  const renderedItems = items.map((item) => {
+  const [expandedIndex, setExpandedIndex] = useState(0);
+
+  const renderedItems = items.map((item, index) => {
+    const isExpanded = index === expandedIndex;
+    if (isExpanded) {
+      console.log("expanded");
+    } else {
+      console.log("collapsed");
+    }
     return (
       <div key={item.id}>
-        <div>{item.label}</div>
-        <div>{item.content}</div>
+        <div onClick={() => setExpandedIndex(index)}>{item.label}</div>
+        {isExpanded && <div>{item.content}</div>}
       </div>
     );
   });

208ac5b569bb85682a84e6ca208c72ed730d4506
Haroun Ted <harounted@gmail.com>
2023-10-07 21:18:57 +0300

    finalize dropdown

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ function App() {
   return (
     <div className="flex">
       <DropDown options={options} value={selection} onChange={handleSelect} />
-      <DropDown options={options} value={selection} onChange={handleSelect} />
     </div>
   );
 }

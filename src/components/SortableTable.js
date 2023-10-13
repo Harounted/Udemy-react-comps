8194499c3d9be21aef056cec0a97f28cd73b79ca
IT / Developer Unitas Holding <itdeveloper@Kyla-Macbook-Pro.local>
2023-10-14 00:00:58 +0300

    refactor to use hooks

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -1,22 +1,14 @@
-import { useState } from "react";
 import Table from "./Table";
 import { GoTriangleDown, GoTriangleUp } from "react-icons/go";
+import useSort from "../hooks/use-sort";
 
 function SortableTable(props) {
-  const [sortOrder, setSortOrder] = useState(null);
-  const [sortBy, setSortBy] = useState(null);
   const { config, data } = props;
-  const handleClick = (label) => {
-    if (sortOrder === null) {
-      setSortOrder("asc");
-      setSortBy(label);
-    } else if (sortOrder === "asc") {
-      setSortOrder("desc");
-      setSortBy(label);
-    } else if (sortOrder === "desc") {
-      setSortOrder(null);
-    }
-  };
+  const { sortOrder, sortBy, sortedData, setSortColumn } = useSort(
+    data,
+    config
+  );
+
   const updateconfig = config.map((column) => {
     if (!column.sortValue) {
       return column;
@@ -27,7 +19,7 @@ function SortableTable(props) {
       header: () => (
         <th
           className="cursor-pointer hover:bg-gray-100"
-          onClick={() => handleClick(column.label)}
+          onClick={() => setSortColumn(column.label)}
         >
           <div className="flex items-center">
             {getIcons(column.label, sortBy, sortOrder)}
@@ -38,28 +30,7 @@ function SortableTable(props) {
     };
   });
 
-  let sortedData = data;
-  if (sortOrder && sortBy) {
-    const { sortValue } = config.find((column) => column.label === sortBy);
-    sortedData = [...data].sort((a, b) => {
-      const valueA = sortValue(a);
-      const valueB = sortValue(b);
-
-      const reverseOrder = sortOrder === "asc" ? 1 : -1;
-
-      if (typeof valueA === "string") {
-        return valueA.localeCompare(valueB) * reverseOrder;
-      } else {
-        return (valueA - valueB) * reverseOrder;
-      }
-    });
-  }
-  return (
-    <div>
-      {sortBy} - {sortOrder}
-      <Table {...props} data={sortedData} config={updateconfig} />
-    </div>
-  );
+  return <Table {...props} data={sortedData} config={updateconfig} />;
 }
 
 const getIcons = (label, sortBy, sortOrder) => {
--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,11 +1,14 @@
 import SortableTable from "../components/SortableTable";
+import Table from "../components/Table";
 
 function TablePage() {
   const data = [
     { name: "Orange", color: "bg-orange-500", score: 5 },
     { name: "Apple", color: "bg-red-500", score: 3 },
-    { name: "Banana", color: "bg-yellow-500", score: 1 },
-    { name: "Lime", color: "bg-green-500", score: 4 },
+    { name: "Banana", color: "bg-yellow-300", score: 1 },
+    { name: "Lime", color: "bg-green-200", score: 6 },
+    { name: "Watermelon", color: "bg-green-600", score: 2 },
+    { name: "Strawberry", color: "bg-red-600", score: 10 },
   ];
   const config = [
     {
@@ -21,6 +24,11 @@ function TablePage() {
       label: "Score",
       render: (fruit) => fruit.score,
       sortValue: (fruit) => fruit.score,
+    },
+    {
+      label: "Scores",
+      render: (fruit) => fruit.score * 2,
+      sortValue: (fruit) => fruit.score * 2,
     },
   ];
   const keyFn = (fruit) => {
